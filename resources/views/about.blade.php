e84c042012d9b8319752f48b19f921212fb88f5a
Justin Tazzie <Tazzie02@users.noreply.github.com>
2018-11-21 22:18:33 +1030

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quos animi nesciunt qui eligendi consequuntur nam quasi suscipit sunt, consectetur, veritatis quaerat voluptatibus reprehenderit fugiat possimus optio veniam quisquam aliquid.</p>
+@endsection
